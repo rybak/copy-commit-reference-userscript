8459ba8a3cba1ca2b9b1a5c9b7e2d43ef617e06e
Andrei Rybak <rybak.a.v@gmail.com>
2024-02-02 22:59:00 +0100

    Gitea/Forgejo: add `@match` for code.forgejo.org
    In addition to Forgejo on codeberg.org [1] and Forgejo's dev instance
    next.forgejo.org [2], there is another stable official deployment of
    Forgejo -- code.forgejo.org [3].  Add a corresponding `@match` entry to
    the metadata of gitea-copy-commit-reference.user.js.
    
    [1] Seems to run Forgejo version 1.21.5~0-45-g583d0cb at the moment
    [2] Runs 1.22.0~test at the moment
    [3] Runs 1.21.5~0 at the moment

--- userscripts/gitea-copy-commit-reference.user.js
+++ userscripts/gitea-copy-commit-reference.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gitea: copy commit reference
 // @namespace    https://andrybak.dev
-// @version      4
+// @version      5
 // @license      AGPL-3.0-only
 // @author       Andrei Rybak
 // @description  Adds a "Copy commit reference" button to every commit page on Gitea and Forgejo websites.
@@ -13,6 +13,7 @@
 // @match        https://projects.blender.org/*/commit/*
 // @match        https://codeberg.org/*/commit/*
 // @match        https://next.forgejo.org/*/commit/*
+// @match        https://code.forgejo.org/*/commit/*
 // @require      https://cdn.jsdelivr.net/gh/rybak/userscript-libs@e86c722f2c9cc2a96298c8511028f15c45180185/waitForElement.js
 // @require      https://cdn.jsdelivr.net/gh/rybak/copy-commit-reference-userscript@c7f2c3b96fd199ceee46de4ba7eb6315659b34e3/copy-commit-reference-lib.js
 // @grant        none
