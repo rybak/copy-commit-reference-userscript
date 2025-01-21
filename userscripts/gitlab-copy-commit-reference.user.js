81c2ae55d6aa6738bf92be406e326ee35b75cb37
Andrei Rybak <rybak.a.v@gmail.com>
2025-01-21 16:17:17 +0100

    GitLab: convertPlainSubjectToHtml: use parameter commitHash
    Fix a stupid bug in method convertPlainSubjectToHtml of class GitLab.

--- userscripts/gitlab-copy-commit-reference.user.js
+++ userscripts/gitlab-copy-commit-reference.user.js
@@ -17,7 +17,7 @@
 // ==/UserScript==
 
 /*
- * Copyright (C) 2023 Andrei Rybak
+ * Copyright (C) 2023-2025 Andrei Rybak
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
@@ -138,7 +138,7 @@
 		}
 
 		async convertPlainSubjectToHtml(plainTextSubject, commitHash) {
-			const escapedHtml = await super.convertPlainSubjectToHtml(plainTextSubject, hash);
+			const escapedHtml = await super.convertPlainSubjectToHtml(plainTextSubject, commitHash);
 			if (!escapedHtml.includes('#')) {
 				return escapedHtml;
 			}
